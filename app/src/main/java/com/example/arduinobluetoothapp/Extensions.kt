89d02d6cec80355544b163b5aebc7c0f9b1c4d25
Egorman7 <egorman7@gmail.com>
2019-06-02 16:28:49 +0300

    commit: - fixed roll and heading place

--- app/src/main/java/com/example/arduinobluetoothapp/Extensions.kt
+++ app/src/main/java/com/example/arduinobluetoothapp/Extensions.kt
@@ -13,5 +13,5 @@ fun TextView.writeMessage(tag: String?, messsage: String?){
 
 fun String.parseGyroscopeModel(): GyroscopeModel{
     val data = this.split("/")
-    return GyroscopeModel(data[0].toFloat(), data[1].toFloat(), data[2].toFloat(), data[3].toFloat(), data[4].toFloat(), data[5].toFloat())
+    return GyroscopeModel(data[0].toFloat(), data[1].toFloat(), data[2].toFloat(), data[5].toFloat(), data[4].toFloat(), data[3].toFloat())
 }
